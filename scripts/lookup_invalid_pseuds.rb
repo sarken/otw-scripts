9d5aab955b031800cf7040533ef70fe0d6f562c7
sarken <sarken@gmail.com>
2022-04-04 20:03:48 -0400

    Invalid pseud email (#18)
    * Rename file
    
    * New script
    
    Same as old script, tbh, but with different subject line and prettified a bit.
    
    * Invalid pseud email HTML
    
    TODO: Dates!
    
    * Text version of invalid pseud email
    
    TODO: Dates
    
    * Delete invalid_pseuds_email.pl
    
    * Update drive_email.pl
    
    Prettify and use more generic fallback subject.
    
    * Rename drive_email.pl to simple_email.pl
    
    * Update simple_email.pl
    
    Fix arrow operator
    
    * Update simple_email.pl
    
    Fix unneeded newline
    
    * Create lookup_invalid_pseuds.rb
    
    * Update lookup_invalid_pseuds.rb
    
    * Update simple_email.pl
    
    Use different tidier
    
    * Update lookup_invalid_pseuds.rb
    
    Enclose subject in double quotes in the outputted command
    
    * Update lookup_invalid_pseuds.rb
    
    Update valid types

---
+++ scripts/lookup_invalid_pseuds.rb
@@ -0,0 +1,28 @@
+#!script/rails runner
+#
+# bundle exec rails r script/lookup_invalid_pseuds.rb
+#
+# Use this script to look up pseuds with invalid icon, icon_alt_text, or
+# icon_comment text and prepare to email the users those pseuds belong to.
+
+# Valid icon types, from validates_attachment_content_type in pseuds model.
+# image/jpg is not actually a valid type, but we're going to fix that
+# instead of deleting the icon, so we don't need to email those users.
+valid_types = %w[image/gif image/jpeg image/png image/jpg]
+
+pseuds_with_invalid_icons = Pseud.where("icon_file_name IS NOT NULL AND icon_content_type NOT IN (?)", valid_types)
+pseuds_with_invalid_text = Pseud.where("CHAR_LENGTH(icon_alt_text) > ? OR CHAR_LENGTH(icon_comment_text) > ?", ArchiveConfig.ICON_ALT_MAX, ArchiveConfig.ICON_COMMENT_MAX)
+
+invalid_pseuds = [pseuds_with_invalid_icons, pseuds_with_invalid_text].flatten.uniq
+invalid_pseuds_count = invalid_pseuds.count
+
+# Get list of users to email.
+File.open("/tmp/runme.sh", "w") do |f|
+  users_with_invalid_pseuds = invalid_pseuds.map(&:user).uniq
+
+  users_with_invalid_pseuds.each do |user|
+    next if user.nil?
+    subject = "[AO3] Issue with your AO3 pseud icon"
+    f.puts "perl scripts/invalid_pseuds_email.pl -t config/email_templates/2022-03-invalid-pseuds -s \"#{subject}\" -e #{user.email} -u #{user.login}\n"
+  end
+end
